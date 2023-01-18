13218daec402053383d7e38b869d4bbb3065ac1f
cyace84 <cyace84@gmail.com>
2023-01-18 13:05:25 +0100

    added dev pool list

--- env.js
+++ env.js
@@ -17,22 +17,20 @@ export default {
       secretKey: accounts.alice.sk,
       pools: {
         0: {
+          name: "KUSD/KUSD",
+          tickIndex: 0,
           tokenX: {
-            token_type: "FA2",
-            token_id: 0,
-            token_address: "KT1J8Hr3BP8bpbfmgGpRPoC9nAMSYtStZG43",
+            fa12: "KT1Q1qqJAzzxzGzQB6RmxnAKgxttf7Hntceg",
           },
 
           tokenY: {
-            token_type: "FA2",
-            token_id: 0,
-            token_address: "KT1X125rpfx7v2jxApYKQknh5gkkDWHJVuCn",
+            fa12: "KT1EjAaRRQpaZrbDEcEG6aEGuydwUkAtXsBU",
           },
-          feeBPS: 10,
+          feeBPS: 100,
           metadata: MichelsonMap.fromLiteral({
-            name: Buffer.from("Test Pool", "ascii").toString("hex"),
+            name: Buffer.from("KUSD/KUSD", "ascii").toString("hex"),
             description: Buffer.from("Test Pool", "ascii").toString("hex"),
-            symbol: Buffer.from("TEST", "ascii").toString("hex"),
+            symbol: Buffer.from("KUSD/KUSD", "ascii").toString("hex"),
             shouldPreferSymbol: Buffer.from("true", "ascii").toString("hex"),
             thumbnailUri: Buffer.from(
               "https://i.imgur.com/1J8Hr3B.png",
@@ -41,22 +39,74 @@ export default {
           }),
         },
         1: {
+          name: "KUSD/Tea",
+          tickIndex: 10986,
+          tokenX: { fa12: "KT1GG8Zd5rUp1XV8nMPRBY2tSyVn6NR5F4Q1" },
+
+          tokenY: {
+            fa2: {
+              token_id: 1,
+              token_address: "KT18uv7PtGedfudtG7QwFTrDBRjdLe5qj2my",
+            },
+          },
+          feeBPS: 50,
+          metadata: MichelsonMap.fromLiteral({
+            name: Buffer.from("KUSD/Tea", "ascii").toString("hex"),
+            description: Buffer.from("Test Pool 2", "ascii").toString("hex"),
+            symbol: Buffer.from("KUSD/Tea", "ascii").toString("hex"),
+            shouldPreferSymbol: Buffer.from("true", "ascii").toString("hex"),
+            thumbnailUri: Buffer.from(
+              "https://i.imgur.com/1J8Hr3B.png",
+              "ascii",
+            ).toString("hex"),
+          }),
+        },
+        2: {
+          name: "QUIPU/KUSD",
+          tickIndex: -6931,
           tokenX: {
-            token_type: "FA2",
-            token_id: 0,
-            token_address: "KT19363aZDTjeRyoDkSLZhCk62pS4xfvxo6c",
+            fa2: {
+              token_id: 0,
+              token_address: "KT19363aZDTjeRyoDkSLZhCk62pS4xfvxo6c",
+            },
           },
 
           tokenY: {
-            token_type: "FA2",
-            token_id: 0,
-            token_address: "KT1X125rpfx7v2jxApYKQknh5gkkDWHJVuCn",
+            fa12: "KT1Wgp6qSsDN7mCaDk5XDEQU52MezE8B9mr5",
           },
           feeBPS: 10,
           metadata: MichelsonMap.fromLiteral({
-            name: Buffer.from("Test Pool 2", "ascii").toString("hex"),
+            name: Buffer.from("QUIPU/KUSD", "ascii").toString("hex"),
             description: Buffer.from("Test Pool 2", "ascii").toString("hex"),
-            symbol: Buffer.from("TEST2", "ascii").toString("hex"),
+            symbol: Buffer.from("QUIPU/KUSD", "ascii").toString("hex"),
+            shouldPreferSymbol: Buffer.from("true", "ascii").toString("hex"),
+            thumbnailUri: Buffer.from(
+              "https://i.imgur.com/1J8Hr3B.png",
+              "ascii",
+            ).toString("hex"),
+          }),
+        },
+        3: {
+          name: "Grape/uUSD",
+          tickIndex: 19460,
+          tokenX: {
+            fa2: {
+              token_id: 0,
+              token_address: "KT18uv7PtGedfudtG7QwFTrDBRjdLe5qj2my",
+            },
+          },
+
+          tokenY: {
+            fa2: {
+              token_id: 0,
+              token_address: "KT1N4NfnYmJucXYkuPdvJG4Jxbz3TetCTqJc",
+            },
+          },
+          feeBPS: 500,
+          metadata: MichelsonMap.fromLiteral({
+            name: Buffer.from("Grape/uUSD", "ascii").toString("hex"),
+            description: Buffer.from("Test Pool 4", "ascii").toString("hex"),
+            symbol: Buffer.from("Grape/uUSD", "ascii").toString("hex"),
             shouldPreferSymbol: Buffer.from("true", "ascii").toString("hex"),
             thumbnailUri: Buffer.from(
               "https://i.imgur.com/1J8Hr3B.png",
@@ -73,22 +123,20 @@ export default {
       secretKey: accounts.dev.sk,
       pools: {
         0: {
+          name: "KUSD/KUSD",
+          tickIndex: -69081,
           tokenX: {
-            token_type: "FA2",
-            token_id: 0,
-            token_address: "KT19363aZDTjeRyoDkSLZhCk62pS4xfvxo6c",
+            fa12: "KT1Q1qqJAzzxzGzQB6RmxnAKgxttf7Hntceg",
           },
 
           tokenY: {
-            token_type: "FA1",
-            token_id: 0,
-            token_address: "KT1GG8Zd5rUp1XV8nMPRBY2tSyVn6NR5F4Q1",
+            fa12: "KT1EjAaRRQpaZrbDEcEG6aEGuydwUkAtXsBU",
           },
-          feeBPS: 10,
+          feeBPS: 100,
           metadata: MichelsonMap.fromLiteral({
-            name: Buffer.from("QSGov/Kolibri", "ascii").toString("hex"),
-            description: Buffer.from("QSGov/Kolibri", "ascii").toString("hex"),
-            symbol: Buffer.from("QSGovKolibri", "ascii").toString("hex"),
+            name: Buffer.from("KUSD/KUSD", "ascii").toString("hex"),
+            description: Buffer.from("Test Pool", "ascii").toString("hex"),
+            symbol: Buffer.from("KUSD/KUSD", "ascii").toString("hex"),
             shouldPreferSymbol: Buffer.from("true", "ascii").toString("hex"),
             thumbnailUri: Buffer.from(
               "https://i.imgur.com/1J8Hr3B.png",
@@ -97,22 +145,74 @@ export default {
           }),
         },
         1: {
+          name: "KUSD/Tea",
+          tickIndex: -219283,
+          tokenX: { fa12: "KT1GG8Zd5rUp1XV8nMPRBY2tSyVn6NR5F4Q1" },
+
+          tokenY: {
+            fa2: {
+              token_id: 1,
+              token_address: "KT18uv7PtGedfudtG7QwFTrDBRjdLe5qj2my",
+            },
+          },
+          feeBPS: 50,
+          metadata: MichelsonMap.fromLiteral({
+            name: Buffer.from("KUSD/Tea", "ascii").toString("hex"),
+            description: Buffer.from("Test Pool 2", "ascii").toString("hex"),
+            symbol: Buffer.from("KUSD/Tea", "ascii").toString("hex"),
+            shouldPreferSymbol: Buffer.from("true", "ascii").toString("hex"),
+            thumbnailUri: Buffer.from(
+              "https://i.imgur.com/1J8Hr3B.png",
+              "ascii",
+            ).toString("hex"),
+          }),
+        },
+        2: {
+          name: "QUIPU/KUSD",
+          tickIndex: 269392,
           tokenX: {
-            token_type: "FA2",
-            token_id: 0,
-            token_address: "KT19363aZDTjeRyoDkSLZhCk62pS4xfvxo6c",
+            fa2: {
+              token_id: 0,
+              token_address: "KT19363aZDTjeRyoDkSLZhCk62pS4xfvxo6c",
+            },
           },
 
           tokenY: {
-            token_type: "FA2",
-            token_id: 0,
-            token_address: "KT1N4NfnYmJucXYkuPdvJG4Jxbz3TetCTqJc",
+            fa12: "KT1Wgp6qSsDN7mCaDk5XDEQU52MezE8B9mr5",
           },
           feeBPS: 10,
           metadata: MichelsonMap.fromLiteral({
-            name: Buffer.from("QSGov/Youves", "ascii").toString("hex"),
-            description: Buffer.from("QSGov/Youves", "ascii").toString("hex"),
-            symbol: Buffer.from("QSGov/Youves", "ascii").toString("hex"),
+            name: Buffer.from("QUIPU/KUSD", "ascii").toString("hex"),
+            description: Buffer.from("Test Pool 2", "ascii").toString("hex"),
+            symbol: Buffer.from("QUIPU/KUSD", "ascii").toString("hex"),
+            shouldPreferSymbol: Buffer.from("true", "ascii").toString("hex"),
+            thumbnailUri: Buffer.from(
+              "https://i.imgur.com/1J8Hr3B.png",
+              "ascii",
+            ).toString("hex"),
+          }),
+        },
+        3: {
+          name: "Grape/uUSD",
+          tickIndex: 152025,
+          tokenX: {
+            fa2: {
+              token_id: 0,
+              token_address: "KT18uv7PtGedfudtG7QwFTrDBRjdLe5qj2my",
+            },
+          },
+
+          tokenY: {
+            fa2: {
+              token_id: 0,
+              token_address: "KT1N4NfnYmJucXYkuPdvJG4Jxbz3TetCTqJc",
+            },
+          },
+          feeBPS: 500,
+          metadata: MichelsonMap.fromLiteral({
+            name: Buffer.from("Grape/uUSD", "ascii").toString("hex"),
+            description: Buffer.from("Test Pool 4", "ascii").toString("hex"),
+            symbol: Buffer.from("Grape/uUSD", "ascii").toString("hex"),
             shouldPreferSymbol: Buffer.from("true", "ascii").toString("hex"),
             thumbnailUri: Buffer.from(
               "https://i.imgur.com/1J8Hr3B.png",
